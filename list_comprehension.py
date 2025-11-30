40704cc46e2d30fa32df6b7e0d62c75afaa973fd
manoj siripuram <manojsiripuram248@gmail.com>
2025-11-30 16:40:02 -0700

    updated_code

--- list_comprehension.py
+++ list_comprehension.py
@@ -1,3 +1,8 @@
+
+
+
+
+
 temp_list = []
 for x in [1, 2, 3, 4]:
     temp_list.append(x * 4)
@@ -5,3 +10,5 @@ print("final_result:", temp_list)
 
 result = [x * 4 for x in [1, 2, 3, 4]]
 print("final_result_using_list_comprehension:", result)
+
+# added changes 1
