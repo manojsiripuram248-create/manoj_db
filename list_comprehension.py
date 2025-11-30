4f7a8dfc32b4bac638fdd8786134a7bea864992d
manoj siripuram <manojsiripuram248@gmail.com>
2025-11-30 00:00:49 -0700

    First commit

---
+++ list_comprehension.py
@@ -0,0 +1,7 @@
+temp_list = []
+for x in [1, 2, 3, 4]:
+    temp_list.append(x * 4)
+print("final_result:", temp_list)
+
+result = [x * 4 for x in [1, 2, 3, 4]]
+print("final_result_using_list_comprehension:", result)
---
+++ test.py
@@ -0,0 +1,16 @@
+temp_list = []
+for x in [1, 2, 3, 4]:
+    temp_list.append(x * 2)
+result = temp_list
+print(result)
+
+
+
+
+temp_list = []
+for x in [1, 2, 3, 4]:
+    temp_list.append(x * 2)
+result = temp_list
+print(result)
+
+
